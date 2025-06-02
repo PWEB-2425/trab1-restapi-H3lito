3c9d896b7ffd21577308c15f0a62fe5270e1ede8
H3lito <helitohorta@ipvc.pt>
2025-06-02 17:41:11 +0100

    .

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,16 @@
 // JS para operações CRUD com Fetch API
+
+document.getElementById("verAlunos").addEventListener("click", listarAlunos);
+async function listarAlunos(params) {
+   const apiUrl = 'http://localhost:3000/alunos'; 
+
+   const resposta = await fetch(apiUrl);
+   console.log(resposta);
+   const alunosJS = await resposta.json();
+   console.log(alunosJS);
+   let ulalunos = document.getElementById(listaAlunos);
+   console.log(ulalunos);
+    ulalunos.innerHTML = ''; // Limpa a lista antes de adicionar novos itens
+
+}
+
