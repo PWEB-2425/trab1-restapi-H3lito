8cf98f184ad67c615861c8c963825ffd12464b3e
H3lito <helitohorta@ipvc.pt>
2025-06-26 22:45:42 +0100

    resolução de conflito

--- frontend/script.js
+++ frontend/script.js
@@ -96,39 +96,3 @@ async function adicionarCurso(e) {
     alert('Erro ao registar curso.');
   }
 }
-
-
- //document.addEventListener('DOMContentLoaded', function() {
-  //document.getElementById('btnAlunos').addEventListener('click', function() {
-           // document.getElementById('divAlunos').style.display = 'block';
-       // });
-
-   // });
-
-   // listarAlunos();
-
-//async function listarAlunos() {
-   // const url = "http://localhost:3000/alunos";
-   // const resposta = await fetch(url);
-   // const alunos = await resposta.json();
-   // const tbody = document.getElementById("tbodyAlunos");
-    
-   // tbody.innerHTML = "";
-
-   // alunos.forEach(aluno => {
-    //    tbody.innerHTML += `
-            //<tr>
-             //   <td>${aluno.nome}</td>
-               // <td>${aluno.apelido}</td>
-               // <td>${aluno.curso}</td>
-               // <td>${aluno.anoCurricular}</td>
-               // <td>${aluno.idade}</td>
-            //</tr>
-        //`;
-   // });
-//}
-//document.addEventListener('DOMContentLoaded', function() {
-   // document.getElementById("btnAddAluno").addEventListener("click", function() {
-        //document.getElementById("alunoForm").style.display = "block";
-   // });
-//});//
